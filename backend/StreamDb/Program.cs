a4fb5efc64e639549c5199fcc3253025a23663a4
Iradukundapaci <paifique57@gmail.com>
2025-01-26 07:54:47 +0200

    fix: Adding fixed port

--- backend/StreamDb/Program.cs
+++ backend/StreamDb/Program.cs
@@ -1,18 +1,25 @@
+using Microsoft.EntityFrameworkCore;
 using StreamDb.Context;
 using StreamDb.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddNpgsql<StreamDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
-builder.Services.AddGrpc();
+builder.WebHost.UseKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000);
+});
 
+builder.Services.AddDbContext<StreamDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 var app = builder.Build();
 
+app.MapGrpcReflectionService();
 app.MapGrpcService<UserService>();
 app.MapGrpcService<StreamService>();
 app.MapGrpcService<CommentService>();
 app.MapGet("/",
-    () =>
-        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    () => "Communication with gRPC endpoints must be made through a gRPC client.");
 
 app.Run();
